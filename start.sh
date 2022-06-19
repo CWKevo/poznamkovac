faaa1cb6742ca366f3784eb69d4edd0bd954d61e
Kevo <me@kevo.link>
2022-06-19 09:51:50 +0200

    Vylepšené lepšenie

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ export FLASK_APP="poznamkovac.web:WEB"
 (flask run --host 0.0.0.0 --port 5000 & uvicorn poznamkovac.web.api:API --host 0.0.0.0 --port 5001)
 
 echo ""
-read -p "Press [Enter] to continue..."
+read -p "[Enter] pre ukončenie"
