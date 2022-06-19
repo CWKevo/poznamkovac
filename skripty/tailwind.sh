d41b709ab63ffa2d6a9b0506e1b3abfa2e95b53f
Kevo <me@kevo.link>
2022-06-19 10:26:27 +0200

    Tailwind

--- skripty/tailwind.sh
+++ skripty/tailwind.sh
@@ -1,7 +1,7 @@
 cd "$(dirname "$0")/.."
 cd tailwind
 
-npx tailwindcss -i ../poznamkovac/web/static/css/tailwind_zaklad.css -o ../poznamkovac/web/static/css/produkcia.css
-npx cleancss ../poznamkovac/web/static/css/produkcia.css -o ../poznamkovac/web/static/css/produkcia.min.css
+npx tailwindcss -i ../poznamkovac/web/staticke_subory/css/tailwind_zaklad.css -o ../poznamkovac/web/staticke_subory/css/produkcia.css
+npx cleancss ../poznamkovac/web/staticke_subory/css/produkcia.css -o ../poznamkovac/web/staticke_subory/css/produkcia.min.css
 
-rm ../poznamkovac/web/static/css/produkcia.css
+rm ../poznamkovac/web/staticke_subory/css/produkcia.css
